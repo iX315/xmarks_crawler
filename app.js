37b5071de4807752831c2f242986c68f2a228d05
iX315 <ix315@me.com>
2017-04-17 20:23:54 +0200

    format doc

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const app = express()
 const crawler = require("./crawler.js")
 const log = console.log
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
 app.post("/", (req, res, next) => {
-    var uri = req.body.url
-    if(!uri) next(new Error('Invalid!'))
-    new crawler(uri).start().then((data)=>{
-        res.send(data)
-    })
+  var uri = req.body.url
+  if (!uri) next(new Error('Invalid!'))
+  new crawler(uri).start().then((data) => {
+    res.send(data)
+  })
 })
 
-app.get("/",(req, res, next) => {
-    res.send("Error")
-}) 
+app.get("/", (req, res, next) => {
+  res.send("Error")
+})
 
 app.use((req, res, next) => {
   var err = new Error('Not Found')
